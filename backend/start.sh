1091ae8bce07f171999fe9f20a0b34a995cc908c
tanish35 <tanishmajumdar2912@gmail.com>
2025-06-24 14:32:51 +0530

    fix docker issues

---
+++ backend/start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Make sure DATABASE_URL is visible
+echo "📦 DATABASE_URL: $DATABASE_URL"
+
+# Run Prisma
+prisma generate
+
+# Start the app
+uvicorn main:app --host 0.0.0.0 --port 8000
